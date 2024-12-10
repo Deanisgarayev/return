66b7ff88a7c18bf427dd0028361d866c7aec0c4b
Deanisgarayev <garaevdenis2019@gmail.com>
2024-12-10 14:06:21 +0500

    return1

---
+++ main.py
@@ -0,0 +1,25 @@
+from itertools import count
+
+
+def send_email(message, recipient, sender="university.help@gmail.com"):
+    end = (".com", ".ru", ".net")
+    valid = recipient.endswith(end, -4) == sender.endswith(end, -4)
+    if recipient.count("@") == 0 or sender.count("@") == 0 or valid == False:
+        print("Невозможно отправить письмо с адреса <sender> на адрес <recipient>")
+    elif recipient == sender:
+        print("Нельзя отправить письмо самому себе!")
+    elif sender == 'university.help@gmail.com':
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
+    else:
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
+
+
+
+
